5c9ea015831b19248d8da6a559f1d877f3c5d767
Natali <tasha3425@mail.ru>
2023-07-05 21:50:56 +0300

    Add console.log

--- Homework3/js/app.js
+++ Homework3/js/app.js
@@ -3,10 +3,12 @@ function add() {
     let name = document.getElementById("nameElement").value
     document.getElementById("resultName").innerText = "Привет, " + name + " !";
     document.getElementById("nameElement").value = '';
+    console.log(resultName);
 }
 
 function app() {
     let age = document.getElementById("ageElement").value
     document.getElementById("result").innerText = "Вам " + age + " лет";
     document.getElementById("ageElement").value = '';
+    console.log(result);
 }
